0c6842319f21f9349820de870e995e03cf3877f0
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-10-01 14:07:39 +0200

    Extension: added edit for already hiredPerson

--- src/pages/PersonProfile/components/HireForm.jsx
+++ src/pages/PersonProfile/components/HireForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 function HireForm( {person, hiredPeople, setHiredPeople} ) {
@@ -6,28 +6,46 @@ function HireForm( {person, hiredPeople, setHiredPeople} ) {
   const navigate = useNavigate()
 
   function handleSubmit(event) {
-    event.preventDefault() 
+    event.preventDefault()
     person.wage = wage;
-    
+    person.hired = true;
+
     setHiredPeople([...hiredPeople, person])
     console.log('added' + hiredPeople)
     navigate('/')
     setWage(0)
   }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="wage">Wage Offer</label>
-      <input
-        type="text"
-        id="wage"
-        name="wage"
-        onChange={e => setWage(e.target.value)}
-        value={wage}
-      />
-      <button type="submit">Hire</button>
-    </form>
-  )
+  if(!person.hired){
+    return (
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="wage">Wage Offer</label>
+        <input
+          type="text"
+          id="wage"
+          name="wage"
+          onChange={e => setWage(e.target.value)}
+          value={wage}
+        />
+        <button type="submit">Hire</button>
+      </form>
+    )
+  }
+  else{
+    return (
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="wage">Wage Offer</label>
+        <input
+          type="text"
+          id="wage"
+          name="wage"
+          onChange={e => setWage(e.target.value)}
+          value={wage}
+        />
+        <button type="submit">Edit</button>
+      </form>
+    )
+  }
 }
-
+  
 export default HireForm
